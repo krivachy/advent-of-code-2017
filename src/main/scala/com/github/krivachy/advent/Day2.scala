87b4284bc54c176dd21fff183fa41bc3421c1320
Akos Krivachy <akos.krivachy@futurice.com>
2017-12-02 13:37:00 +0000

    Day 2 part 2 as well

--- src/main/scala/com/github/krivachy/advent/Day2.scala
+++ src/main/scala/com/github/krivachy/advent/Day2.scala
@@ -11,8 +11,33 @@ object Day2 extends App {
     }
   }
 
+  private def tryDivisibilityOneWay(a: Int, b: Int): Option[Int] = {
+    if (a % b == 0) Some(a / b) else None
+  }
+
+  private def tryDivisibility(a: Int, b: Int): Option[Int] = {
+    tryDivisibilityOneWay(a, b)
+      .orElse(tryDivisibilityOneWay(b, a))
+  }
+
+  def solutionPart2(input: Seq[Seq[Int]]): Int = {
+    input.foldLeft(0) {
+      case (sum, nextRow) =>
+        sum + nextRow
+          // take all combinations of pairs
+          .combinations(2)
+          // try divisibility both ways and only keep the ones where it succeeded
+          .flatMap { case a :: b :: Nil => tryDivisibility(a, b) }
+          .toList
+          // take first one that succeeded
+          .headOption
+          .getOrElse(0)
+    }
+  }
+
 
   val problemInput = Source.fromResource("day2.txt").getLines.map(_.split('\t').map(_.toInt).toList).toList
   println(solutionPart1(problemInput))
+  println(solutionPart2(problemInput))
 
 }
--- src/test/scala/com/github/krivachy/advent/Day2Test.scala
+++ src/test/scala/com/github/krivachy/advent/Day2Test.scala
@@ -1,15 +1,26 @@
 package com.github.krivachy.advent
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest.{Matchers, WordSpec}
 
-class Day2Test extends FlatSpec with Matchers {
+class Day2Test extends WordSpec with Matchers {
 
-  "The day 2 solution" should "pass the example" in {
-    Day2.solutionPart1(Seq(
-      Seq(5, 1, 9, 5),
-      Seq(7, 5, 3),
-      Seq(2, 4, 6, 8)
-    )) shouldBe 18
+  "The day 2 part 1 solution" should {
+    "pass the example" in {
+      Day2.solutionPart1(Seq(
+        Seq(5, 1, 9, 5),
+        Seq(7, 5, 3),
+        Seq(2, 4, 6, 8)
+      )) shouldBe 18
+    }
+  }
+  "The day 2 part 2 solution" should {
+    "pass the example" in {
+      Day2.solutionPart2(Seq(
+        Seq(5, 9, 2, 8),
+        Seq(9, 4, 7, 3),
+        Seq(3, 8, 6, 5)
+      )) shouldBe 9
+    }
   }
 
 }
