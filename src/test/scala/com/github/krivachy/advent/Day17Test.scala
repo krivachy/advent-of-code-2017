c1977c4912ae3bb846d328bc1b43d55b605fef08
Akos Krivachy <akos.krivachy@futurice.com>
2017-12-17 16:24:07 +0000

    Day 17 solution

---
+++ src/test/scala/com/github/krivachy/advent/Day17Test.scala
@@ -0,0 +1,18 @@
+package com.github.krivachy.advent
+
+import org.scalatest.{Matchers, WordSpec}
+
+class Day17Test extends WordSpec with Matchers {
+
+  "Day 17 part 1 solution" should {
+    "pass the example" in {
+      Day17.solutionPart1(3, 2017) shouldBe 638
+    }
+  }
+  "Day 17 part 2 solution" should {
+    "pass the my input and solution" in {
+      Day17.solutionPart1(335, 50000000) shouldBe 27650600
+    }
+  }
+
+}
