5cb1002b7124b8a0022722883d6428de03071d57
Akos Krivachy <akos.krivachy@futurice.com>
2017-12-21 18:30:59 +0100

    Fix infinite loop issue in day17 tests

--- src/test/scala/com/github/krivachy/advent/Day17Test.scala
+++ src/test/scala/com/github/krivachy/advent/Day17Test.scala
@@ -8,10 +8,13 @@ class Day17Test extends WordSpec with Matchers {
     "pass the example" in {
       Day17.solutionPart1(3, 2017) shouldBe 638
     }
+    "pass my input" in {
+      Day17.solutionPart1(335, 2017) shouldBe 1282
+    }
   }
   "Day 17 part 2 solution" should {
     "pass the my input and solution" in {
-      Day17.solutionPart1(335, 50000000) shouldBe 27650600
+      Day17.solutionPart2(335, 50000000) shouldBe 27650600
     }
   }
 
